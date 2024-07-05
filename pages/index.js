90c9ab8204b9fd52c1708b1f2b7acfae25e5a39d
Isabell Urban <mail@isabellurban.com>
2024-07-05 10:57:15 +0200

    add fetch

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((response) => response.json());
+
 export default function HomePage() {
+  const { data, isLoading, error } = useSWR(
+    `https://example-apis.vercel.app/api/art`,
+    fetcher
+  );
+  console.log("data", data);
+
+  if (error) return <h1>failed to load</h1>;
+
+  if (isLoading) return <h1>loading...</h1>;
+
   return (
     <div>
       <h1>Art Pieces App of RAI!</h1>
